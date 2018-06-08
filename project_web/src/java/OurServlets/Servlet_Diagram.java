b36b8b78c393a6fe07615db9855e736986ba6218
aesquivelv <albertoesquivel.97@gmail.com>
2018-06-08 13:45:16 -0500

    Fati bb <3

--- project_web/src/java/OurServlets/Servlet_Diagram.java
+++ project_web/src/java/OurServlets/Servlet_Diagram.java
@@ -14,6 +14,7 @@ import org.jdom.input.SAXBuilder;
 import static Utilities.OurXML.*;
 import Utilities.User;
 import java.io.FileOutputStream;
+import javax.servlet.http.HttpSession;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 
@@ -22,22 +23,17 @@ public class Servlet_Diagram extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
        throws ServletException, IOException{
-       Integer elementos=Integer.parseInt(request.getParameter("num_elementos"));
-       String texto=request.getParameter("texto");
        
+       HttpSession session=request.getSession();
+       
+       Integer elementos=Integer.parseInt(String.valueOf(session.getAttribute("num_elementos")));
+       String texto=String.valueOf(session.getAttribute("texto"));
        PrintWriter pw=response.getWriter();
-       String ruta = request.getRealPath("");
-       ruta += "\\fabric.min.js";
-       
-        System.out.println("Path:"+ruta);
        pw.println("<!DOCTYPE html>");
        pw.println("<html>");
        pw.println("<head>");
        pw.println("<title>Diagrama</title>");
-       //pw.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.1.0/fabric.all.min.js\" ></script>");
-       //pw.println("<script src=\""+ruta.replace("\\build\\","\\")+"\"></script>");
        pw.println("<script src=\"fabric.min.js\"></script>");
-       pw.println("<script src='https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>");
        pw.println("</head>");
        pw.println("<body> ");
        
@@ -45,22 +41,24 @@ public class Servlet_Diagram extends HttpServlet {
        pw.println("<canvas id='mcanvas' width='"+300*elementos+"' height='600'></canvas>");
        pw.println("<script type=\"text/javascript\">");
        pw.println("var canvas = new fabric.Canvas('mcanvas');");
+       
        pw.println("var text=new fabric.Text('"+texto+"',{left:300,top:10,fontSize:14});");
        pw.println("canvas.add(text);");
-       pw.println("text.set('selectable',true);");
+       pw.println("text.set('selectable',false);");
         for (int i = 0; i < elementos; i++) {
             /*Crear whitespaces*/
             pw.println("var rect"+i+" = new fabric.Rect({");
-            pw.println("left:"+left+",top:"+top+",fill:'#1e90ff',width:150,height:100});");
+            pw.println("left:"+left+",top:"+top+",fill:'#e0fcfc',width:150,height:100});");
             pw.println("canvas.add(rect"+i+");");
-            pw.println("rect"+i+".set('selectable',true);");
+            pw.println("rect"+i+".set('selectable',false);");
             /*Crear objetos a mover*/
             pw.println("var recta"+i+" = new fabric.Rect({");
-            pw.println("left:"+left+",top:"+(top-200)+",fill:'black',width:150,height:100});");
+            pw.println("left:"+left+",top:"+(top-200)+",fill:'#0098FF',width:150,height:100});");
+            pw.println("recta"+i+".set('selectable',true);");
+            pw.println("var texto"+i+" = new fabric.Text('"+request.getParameter("txt"+i)+"',{left:"+(left+10)+",top:"+(top-180)+",fontSize:14});");
+            pw.println("canvas.add(texto"+i+");");
             pw.println("canvas.add(recta"+i+");");
-            pw.println("recta"+i+".set('selectable',false);");
-            
-            pw.println("var group = new fabric.Group([ texto"+i+", recta"+i+" ], {\n" +
+            pw.println("var group = new fabric.Group([ recta"+i+", texto"+i+" ], {\n" +
                   "  left: "+left+",\n" +
                   "  top: "+(top-200)+",\n" +
                   "});\n" +
@@ -70,11 +68,12 @@ public class Servlet_Diagram extends HttpServlet {
             left+=200;
         }
        
-       pw.println("function myFunction(){console.log(JSON.stringify(canvas));}");
+       pw.println("function myFunction(){document.getElementById('textarea1').value=JSON.stringify(canvas);console.log(JSON.stringify(canvas));}");
        
        
        pw.println("</script>");
-       pw.println("<input class=\"button b1\" type=\"button\" value=\"Agregar\" onclick=\"myFunction();\"/>");
+       pw.println("<input class='button b1' type='button' value='Guardar estado' onclick='myFunction();'/>");
+       pw.println("<textarea id='textarea1'></textarea>");
        pw.println("<input type='submit' name='Enviar'>");
        pw.println("</body>");
        
--- project_web/src/java/OurServlets/Servlet_get_text.java
+++ project_web/src/java/OurServlets/Servlet_get_text.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Servlet_get_text extends HttpServlet {
     
@@ -15,16 +16,21 @@ public class Servlet_get_text extends HttpServlet {
         
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        int n = Integer.parseInt((String)request.getAttribute(""));
+        Integer n=Integer.parseInt(request.getParameter("num_elementos"));
+        String texto=request.getParameter("texto");
+        HttpSession session = request.getSession();
+        session.setAttribute("num_elementos", String.valueOf(n));
+        session.setAttribute("texto",texto);
         
-        out.println("!DOCTYPE html>");
+        
+        out.println("<!DOCTYPE html>");
         out.println("<html>");
             out.println("<head>");
                 out.println("<title>Servlet get text</title>");
             out.println("</head>");
             
             out.println("<body>");
-                out.println("<form action='' method='GET'>");
+                out.println("<form action='Servlet_Diagram' method='GET'>");
                     for(int i = 0; i < n; i++)
                         out.println("Ingrese texto: <input type='text' name='txt"+i+"'/><br/>");
                     out.println("<input type='submit'/>");
