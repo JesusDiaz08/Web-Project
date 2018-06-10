b2a5a780c11b490544c0c7baaa8635ffe10ad449
aesquivelv <albertoesquivel.97@gmail.com>
2018-06-10 15:06:16 -0500

    Diagrama usando Konva
    Falta guardar la serializacion en el XML y recuperarla

--- project_web/src/java/OurServlets/Servlet_Diagram.java
+++ project_web/src/java/OurServlets/Servlet_Diagram.java
@@ -36,48 +36,82 @@ public class Servlet_Diagram extends HttpServlet {
        pw.println("<head>");
        pw.println("<title>Diagrama</title>");
        pw.println("<link rel=\"stylesheet\" href='back_end/css/style_diagram.css'>");
-       pw.println("<script src=\"fabric.min.js\"></script>");
+       pw.println("<script src=\"konva.min.js\"></script>");
+       pw.println("<meta charset=\"utf-8\">");
        pw.println("</head>");
        pw.println("<body> ");
+       pw.println("<div id=\"container\"></div>");
+       pw.println("<script>");
+       pw.println("function writeMessage(message) {\n" +
+                    "        text.setText(message);\n" +
+                    "        layer.draw();\n" +
+                    "    }");
+       pw.println("var width = window.innerWidth;");
+       pw.println("var height = window.innerHeight;");
+       pw.println("var stage = new Konva.Stage({\n" +
+                    "        container: 'container',\n" +
+                    "        width: width,\n" +
+                    "        height: height\n" +
+                    "    });"); 
+       pw.println(" var layer = new Konva.Layer();");
+       /*Insertar texto que ingresa el profesor*/
+       pw.println("var text = new Konva.Text({\n" +
+"        x: 10,\n" +
+"        y: 10,\n" +
+"        fontFamily: 'Calibri',\n" +
+"        fontSize: 24,\n" +
+"        text: '"+texto+"',\n" +
+"        fill: 'black'\n" +
+"    });");
        
-       Integer left=50,top=400;
-       pw.println("<canvas id='mcanvas' width='"+300*elementos+"' height='600'></canvas>");
-       pw.println("<script type=\"text/javascript\">");
-       pw.println("var canvas = new fabric.Canvas('mcanvas');");
-       
-       pw.println("var text=new fabric.Text('"+texto+"',{left:300,top:10,fontSize:14});");
-       pw.println("canvas.add(text);");
-       pw.println("text.set('selectable',false);");
+       Integer x=10,y=200;
         for (int i = 0; i < elementos; i++) {
-            /*Crear whitespaces*/
-            pw.println("var rect"+i+" = new fabric.Rect({");
-            pw.println("left:"+left+",top:"+top+",fill:'#e0fcfc',width:150,height:100,selectable:false});");
-            pw.println("canvas.add(rect"+i+");");
-           
+             /*Crear objetos fijos*/
+            pw.println("var box"+i+" = new Konva.Rect({");
+            pw.println("x:"+(x)+",y:"+(y+400)+",");
+            pw.println("offset: [50, 25],");
+            pw.println("width: 200,");
+            pw.println("height: 100,");
+            pw.println("fill: '#e0fcfc',");
+            pw.println("stroke: 'black',");
+            pw.println("strokeWidth: 4");
+            pw.println("});");
+            pw.println("layer.add(box"+i+");");
             /*Crear objetos a mover*/
-            pw.println("var recta"+i+" = new fabric.Rect({");
-            pw.println("left:"+left+",top:"+(top-200)+",fill:'#0098FF',width:150,height:100});");
-            pw.println("recta"+i+".set('selectable',true);");
-            pw.println("var texto"+i+" = new fabric.Text('"+request.getParameter("txt"+i)+"',{left:"+(left+10)+",top:"+(top-180)+",fontSize:14});");
+            pw.println("var box"+i+" = new Konva.Rect({");
+            pw.println("x:"+x+",y:"+y+",");
+            pw.println("offset: [50, 25],");
+            pw.println("width: 200,");
+            pw.println("height: 100,");
+            pw.println("fill: '#0098FF',");
+            pw.println("stroke: 'black',");
+            pw.println("strokeWidth: 4");
+            pw.println("});");
+            pw.println("  var group = new Konva.Group({\n" +
+            "        x: "+x+",\n" +
+            "        y: "+y+", draggable:true"+
+            "    });");
+            pw.println("var text"+i+" = new Konva.Text({x:"+(x+10)+",y:"+(y+10)+",fill:'white',text:'"+request.getParameter("txt"+i)+"'});");
+            pw.println("group.add(box"+i+",text"+i+");");
+            pw.println("layer.add(group);");
+           
             
-            pw.println("var group"+i+" = new fabric.Group([ recta"+i+", texto"+i+" ], {\n" +
-                  "  left: "+left+",\n" +
-                  "  top: "+(top-200)+"\n" +
-                  "});\n" +
-                  "\n" +
-                  "canvas.add(group"+i+");");
-            
-            left+=200;
+            x+=250;
         }
-         /*Cargar imagen*/
-         pw.println("fabric.Image.fromURL('"+imagen+"', function(myImg) {\n" +
-                        " var img1 = myImg.set({ left: 700, top: 0 ,width:200,height:200,selectable:false,center:true});\n" +
-                        " canvas.add(img1); \n" +
-                        "});");
-       
-       pw.println("function myFunction(){document.getElementById('textarea1').value=JSON.stringify(canvas);console.log(JSON.stringify(canvas));}");
+       /*Agregar imagen*/
+       pw.println("var imageObj=new Image();");
+       pw.println("imageObj.onload=function(){");
+       pw.println("var imagen= new Konva.Image({");
+       pw.println("x:500,y:50,image:imageObj,width:106,height:118});");
+       pw.println("layer.add(imagen);");
+       pw.println("stage.add(layer);");
+       pw.println("};");
+       pw.println("layer.add(text);");
+       pw.println("imageObj.src='"+imagen+"';");
+       /*Funcion para serializar*/
+       pw.println("function Serializar(){var json=stage.toJSON();document.getElementById('textarea1').value=json;}");
        pw.println("</script>");
-       pw.println("<input class='button b1' type='button' value='Guardar diagrama' onclick='myFunction();'/>");
+       pw.println("<input class='button b1' type='button' value='Guardar diagrama' onclick='Serializar();'/>");//Agregar al XML a partir de la funcion para serializar
        pw.println("<textarea id='textarea1'></textarea>");
        
        pw.println("</body>");
--- project_web/src/java/OurServlets/Servlet_student_diagram.java
+++ project_web/src/java/OurServlets/Servlet_student_diagram.java
@@ -32,25 +32,18 @@ public class Servlet_student_diagram extends HttpServlet {
        pw.println("<head>");
        pw.println("<title>Diagrama</title>");
        pw.println("<link rel=\"stylesheet\" href='back_end/css/style_diagram.css'>");
-       pw.println("<script src=\"fabric.min.js\"></script>");
-       pw.println("<canvas id='m' width='1000' height='600'></canvas>");
-       pw.println("<script type='text/javascript'>");
-       pw.println("var canvas = new fabric.Canvas('m');");
-       pw.println("var json='';");
-       pw.println("function mFunction(){canvas.clear();}");
-       pw.println("function myFunction(){json=document.getElementById('mtext').value;");
-       pw.println("console.log(json);");
-       pw.println("canvas.loadFromJSON(json);}");
-       pw.println("function cargarSVG(){");
-       pw.println("var contsvg=document.getElementById('mtext').value;");
-       pw.println("console.log(contsvg);");
-       pw.println("fabric.loadSVGFromString(contsvg,function(objects, options) {");//Para cargar desde un SVG (comienza con XML)
-       pw.println("canvas.add.apply(canvas, objects);");
-       pw.println("canvas.renderAll();});}");
+       pw.println("<script src=\"konva.min.js\"></script>");
+       pw.println("<meta charset=\"utf-8\">");
+       pw.println("</head>");
+       pw.println("<body>");
+       pw.println("<div id=\"container\"></div>");
+       pw.println("<script>");
+       /*Funcion para deserilizar canvas*/
+       pw.println("function cargar(){var json=document.getElementById('mtext').value; var stage=Konva.Node.create(json,'container');}");
+       
        pw.println("</script>");
-       pw.println("<input class='button b1' type='button' value='Limpiar canvas' onclick='mFunction();'/>\n" +
-                    "<input class='button b1' type='button' value='Cargar canvas' onclick='myFunction();'/>\n" +
-                    "<input class='button b1' type='button' value='SVG' onclick='cargarSVG();'/>\n" +
+       pw.println("<input class='button b1' type='button' value='Cargar canvas' onclick='cargar();'/>\n" +
+                    
                     "<textarea id='mtext'></textarea>");//De donde carga el SVG o el JSON
        pw.println("</body>");
        
