5379fc84c743d12cab0c8c70f1376a8b9353d326
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-06-10 02:44:05 -0500

    Rich text implementation
    Implementación de texto enriquecido

--- project_web/src/java/OurServlets/Servlet_get_text.java
+++ project_web/src/java/OurServlets/Servlet_get_text.java
@@ -41,6 +41,37 @@ public class Servlet_get_text extends HttpServlet {
         
     }
     
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+        
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        Integer n=Integer.parseInt(request.getParameter("num_elementos"));
+        String texto=request.getParameter("txt-content");
+        HttpSession session = request.getSession();
+        session.setAttribute("num_elementos", String.valueOf(n));
+        session.setAttribute("texto",texto);
+        
+        
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+            out.println("<head>");
+                out.println("<title>Servlet get text</title>");
+            out.println("</head>");
+            System.out.println("Texto: "+texto);
+            out.println("<body>");
+                out.println("<form action='Servlet_Diagram' method='post'>");
+                    for(int i = 0; i < n; i++)
+                        out.println("Ingrese texto: <input type='text' name='txt"+i+"'/><br/>");
+                    out.println("Imagen:<input type='text' name='img'/>");
+                    out.println("<input type='submit'/>");
+                out.println("</form>");
+            out.println("</body>");
+        out.println("</html>");
+        
+    }
+    
     //Login
     //Registro
     //Cursos
